d52d73d9581a3531fb6795091aa2fd44747b30b5
faha1999 <kafahad1999@gmail.com>
2022-02-27 00:27:16 +0600

    complete home

--- app/js/app.js
+++ app/js/app.js
@@ -26,3 +26,19 @@ window.onscroll = () => {
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 };
+
+// home slide
+let slides = document.querySelectorAll('.home .slide');
+let index = 0;
+
+function next() {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}
+
+function prev() {
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
+}
